2d51d18f5733ca52f53824866ddb82509be4a9e9
Hector Urtubia <hector@olark.com>
2017-05-23 10:22:30 -0700

    Adding python flask starter

---
+++ python-flask/flask-olark-webhook.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import urllib
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/olarkwebhook", methods=['POST'])
+def olarkwebhook():
+    payload = json.loads(request.form['data'])
+    print payload
+    return json.dumps({'integrationUrl': 'https://myservice.com/uniqueidfortranscript'})
+
+if __name__ == "__main__":
+    app.run()
